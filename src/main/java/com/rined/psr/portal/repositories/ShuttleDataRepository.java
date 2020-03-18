4a9edb406d082f013f692dabaf8a36fde08bfab4
Anton Petrov <antoxa-ac@list.ru>
2020-03-19 02:06:11 +0300

    refact shuttle data layers

--- src/main/java/com/rined/psr/portal/repositories/ShuttleDataRepository.java
+++ src/main/java/com/rined/psr/portal/repositories/ShuttleDataRepository.java
@@ -1,12 +1,6 @@
 package com.rined.psr.portal.repositories;
 
 import com.rined.psr.portal.model.ShuttleData;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface ShuttleDataRepository extends JpaRepository<ShuttleData, Long> {
-
-    Optional<ShuttleData> findShuttleById(long id);
-
+public interface ShuttleDataRepository extends BaseRepository<ShuttleData, Long> {
 }
--- src/main/java/com/rined/psr/portal/services/ShuttleDataService.java
+++ src/main/java/com/rined/psr/portal/services/ShuttleDataService.java
@@ -1,20 +1,16 @@
 package com.rined.psr.portal.services;
 
+import com.rined.psr.portal.converters.ShuttleDataConverter;
 import com.rined.psr.portal.dto.brief.ShuttleDataBrief;
 import com.rined.psr.portal.dto.fully.ShuttleDataDto;
+import com.rined.psr.portal.model.ShuttleData;
+import com.rined.psr.portal.repositories.ShuttleDataRepository;
+import org.springframework.stereotype.Service;
 
-import java.util.List;
-
-public interface ShuttleDataService {
-
-    void addShuttleData(ShuttleDataBrief brief);
-
-    List<ShuttleDataDto> getAllShuttlesData();
-
-    void updateShuttleData(long id, ShuttleDataDto dto);
-
-    ShuttleDataDto getShuttleById(long id);
-
-    void deleteShuttleDataById(long id);
-
+@Service
+public class ShuttleDataService extends BaseService<ShuttleDataDto, ShuttleDataBrief, ShuttleData, Long,
+        ShuttleDataRepository, ShuttleDataConverter> {
+    public ShuttleDataService(ShuttleDataConverter converter, ShuttleDataRepository repository) {
+        super(converter, repository);
+    }
 }
